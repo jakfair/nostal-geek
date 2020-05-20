59f1c7c535a30d14471d646d9cefd60d71055285
unknown <agathedekerle@yahoo.fr>
2020-05-20 15:55:16 +0200

    defis ?

--- resources/views/defiscontroller/form.blade.php
+++ resources/views/defiscontroller/form.blade.php
@@ -1,5 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-    Formulaire pour créer des défis
+    formulaire de création d'utilisateur
+    <form method="get" id="formDefis" action="/form/addDefis" enctype="multipart/form-data">
+        {{ csrf_field() }}
+        <input type="text" name="defis" placeholder="entrer le defis">
+        <button type="submit">Envoyer</button>
+    </form>
+
 @endsection
