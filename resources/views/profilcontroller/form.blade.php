299358f4d27760cf880e66e824fa9edf5595c143
unknown <agathedekerle@yahoo.fr>
2020-05-20 14:53:47 +0200

    profil utilisateur

---
+++ resources/views/profilcontroller/form.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+    formulaire de création d'utilisateur
+    <form method="get" id="formuser" action="/form/addprofil" enctype="multipart/form-data">
+        {{ csrf_field() }}
+        <input type="email" name="mail" placeholder="adresse mail">
+        <input type="password" id="pass" name="password"
+           minlength="8" required placeholder="mot de passe">
+        <input type="text" name="nom_user" placeholder="Nom">
+        <input type="text" name="age" placeholder="Age">
+        <button type="submit">Envoyer</button>
+    </form>
+@endsection
