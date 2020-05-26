1de3619055a1cd6e5a7e6baf4e492eea23959f3c
jakfair <mael92@live.fr>
2020-05-26 09:28:42 +0200

    test scheduled

---
+++ app/fichestored.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace App;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class fichestored extends Model
+{
+    protected $table = "oeuvrestored";
+    public $timestamps = false;
+}
