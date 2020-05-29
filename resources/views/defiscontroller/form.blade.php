3c980cbbffd7d2c680f5a60a7f47f8e6779c8897
jakfair <mael92@live.fr>
2020-05-29 10:17:29 +0200

    correction form défis

--- resources/views/defiscontroller/form.blade.php
+++ resources/views/defiscontroller/form.blade.php
@@ -2,15 +2,15 @@
     <form method="post" id="formDefis" action="/form/addDefis" enctype="multipart/form-data">
         <h4 id="titleformdefi">Proposez vos propres défis pour vos jeux favoris !</h4>
         {{ csrf_field() }}
-        <input type="text" name="intitule" placeholder="entrer le defis">
+        <input type="text" name="intitule" placeholder="entrer le defis" required>
         <select class="jeu" name="categorie" id="">
             <option value="quotidien">Quotidien</option>
             <option value="hebdomadaire">Hebdomadaire</option>
             <option value="mensuel">Mensuel</option>
         </select>
-        <input type="number" name="points" placeholder="Nombre de points">
+        <input type="number" name="points" placeholder="Nombre de points" required>
         <input type="file" name="icone" accept='image/png, image/jpeg'/>
-        <input type="hidden" name="idjeu" value="{{$fiche->id}}">
+        <input type="hidden" name="idjeu" value="{{$fiche->id}}" required>
         <button type="submit"><img src="/img/next.png"/></button>
     </form>
 
